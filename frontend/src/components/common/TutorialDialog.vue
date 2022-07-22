4ef4eb93f884340d0774a8f310a50b47efbeb81b
kimjihoon <rlawlgns0321@gmail.com>
2022-07-22 18:13:22 +0900

    (feat)Teachable Machine으로 캠 화면의 자세에 반응하는 기능 추가

---
+++ frontend/src/components/common/TutorialDialog.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="tutorial-dialog">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name : 'TutorialDialog'
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend/src/views/MypageView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="mypage">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'mypageView'
+}
+</script>
+
+<style>
+
+</style>
