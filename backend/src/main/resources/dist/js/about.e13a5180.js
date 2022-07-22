f3ad905e2d578f8565c1db3c7fd9a3af9c5b7a30
HongInho <inoh0914@gmail.com>
2022-07-22 14:10:37 +0900

    Back) test: openvidu-vue 연결

---
+++ backend/src/main/resources/dist/js/about.e13a5180.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkfrontend"]=self["webpackChunkfrontend"]||[]).push([[443],{4217:function(n,t,u){u.r(t),u.d(t,{default:function(){return f}});var e=function(){var n=this;n._self._c;return n._m(0)},s=[function(){var n=this,t=n._self._c;return t("div",{staticClass:"about"},[t("h1",[n._v("This is an about page")])])}],r=u(3736),a={},l=(0,r.Z)(a,e,s,!1,null,null,null),f=l.exports}}]);
+//# sourceMappingURL=about.e13a5180.js.map
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import OVView from '../views/OVView.vue'
 
 Vue.use(VueRouter)
 
@@ -17,6 +18,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/OV',
+    name: 'OV',
+    component: OVView
   }
 ]
 
