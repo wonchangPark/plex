ecf453ea03ce27065625d62e233cf63e82545db6
Kim YongHwan <ymin96@naver.com>
2022-07-25 17:57:53 +0900

    FE) feat: 메인페이지 작성중

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,32 +1,24 @@
 <template>
-  <div id="app" :style="{backgroundImage:'url('+require('./assets/background.png')+')'}">
-    <nav>
-      <router-link to="/">Home</router-link>  |
-      <router-link to="/waiting">WaitingRoom</router-link>
-    </nav>
-    <router-view/>
+  <div id="app">
+    <div class="d-flex flex-column">
+      <div>asdf0</div>
+      <div>1234</div>
+    </div>
   </div>
 </template>
-
+ 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Inter', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  width: 100vw;
+  height: 100vh;
+  background-image: url('./assets/background.png');
+  background-size: cover;
+  margin-top: 0;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
