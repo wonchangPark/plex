a428c0cfd830329a08dcec7876b94bbd32af137e
Kim YongHwan <ymin96@naver.com>
2022-07-26 13:32:56 +0900

    FE) feat: 공용 컴포넌트 ContentBox 제작

---
+++ frontend/src/components/common/ContentBox.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="content-box-wrap" v-bind:style="{width: this.width+'vw' ,height: this.height+'vh'}">
+    <div id="content-box">
+        <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:"ContentBox",
+  props:['width', 'height']
+}
+</script>
+
+<style>
+#content-box-wrap{
+    background: rgba(46,41,35,1);
+    padding: 10px;
+    display: flex;
+    opacity: 0.93;
+}
+
+#content-box{
+    background: rgba(46,41,35,1);
+    border: solid 3px rgba(135,96,60,1);
+    width: 100%;
+    height: 100%;
+}
+</style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,15 +1,20 @@
 <template>
   <div>
-    <v-btn class="primary">확인하기</v-btn>
+    <v-btn class="primary" elevation="0">확인하기</v-btn>
+    <ContentBox :width="40" :height="30"> 
+    
+     </ContentBox>
   </div>
 </template>
 
 <script>
+import ContentBox from "@/components/common/ContentBox.vue";
 
-  export default {
-    name: 'HomeView',
+export default {
+  name: "HomeView",
 
-    components: {
-    },
-  }
+  components: {
+    ContentBox,
+  },
+};
 </script>
