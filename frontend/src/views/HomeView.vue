1cebdb3652f16f562ec2ca76ac88606718d8262e
Kim YongHwan <ymin96@naver.com>
2022-07-27 13:37:31 +0900

    FE) fix: 병합 충돌

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,29 +1,29 @@
 <template>
   <div class="home">
     <div class="wrap">
-      <img src="@/assets/logo.png" alt="logo" class="item">
+      <img src="@/assets/logo.png" alt="logo" class="item" />
       <v-btn>
-      <router-link to="/login" class="item">로그인</router-link>
+        <router-link to="/login" class="item">로그인</router-link>
       </v-btn>
 
       <v-btn>
-      <router-link to="/waiting" class="item">대기방</router-link>
+        <router-link to="/waiting" class="item">대기방</router-link>
       </v-btn>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "HomeView",
 
-  components: {
-  },
+  components: {},
 };
 </script>
 
 <style scoped>
 img {
-  width: 30%
+  width: 30%;
 }
 .wrap {
   padding-top: 10%;
