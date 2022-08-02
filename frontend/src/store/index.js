8d618697b9f352805cf11fc29eaa22a16bff5a52
Kim YongHwan <ymin96@naver.com>
2022-08-03 08:45:22 +0900

    FE) feat: roomStore 추가

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,10 +2,12 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import { auth } from './auth.module';
 
+import roomStore from '@/store/modules/roomStore.js';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
-    auth
+    auth,
+    roomStore,
   }
 });
---
+++ frontend/src/store/modules/roomStore.js
@@ -0,0 +1,15 @@
+
+const roomStore = {
+    namespace: true,
+    state: {
+        rooms:[[],[],[]],
+        currentPage: 1,
+        lastPage:3,        
+    },
+    getters:{},
+    mutations:{
+        
+    }
+}
+
+export default roomStore;
