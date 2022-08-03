7743c366d7fb4dc0af7d340ffad3fb8423b18728
Kim YongHwan <ymin96@naver.com>
2022-08-03 09:28:08 +0900

    FE) fix: merge 에러 해결

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -3,11 +3,13 @@ import Vuex from 'vuex';
 import auth from './auth.module';
 import room from './room'
 
+import roomStore from '@/store/modules/roomStore.js';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
     auth,
-    room
+    room,
+    roomStore
   }
 });
