6a56d3ede882bec28c16d010c801568d3de1032d
kimjihoon <rlawlgns0321@gmail.com>
2022-08-09 21:59:19 +0900

    FE) fix : 게임결과 모달창 지속시간 변경 적용 누락 수정

--- frontend/src/views/RoomView.vue
+++ frontend/src/views/RoomView.vue
@@ -258,7 +258,7 @@ export default {
 						if (this.score1 - this.score2 >= 10){
 							this.game.scene.getScene('ropeFightScene').LeftWin();
 							setTimeout(() => this.gameFinished = true, 1000);
-							setTimeout(() => this.gameFinished = false, 5000);
+							setTimeout(() => this.gameFinished = false, 7000);
 						}
 						else{
 							if (this.score1 > this.score2 + 7)
@@ -275,7 +275,7 @@ export default {
 						if (this.score2 - this.score1 >= 10){
 							this.game.scene.getScene('ropeFightScene').RightWin();
 							setTimeout(() => this.gameFinished = true, 1000);
-							setTimeout(() => this.gameFinished = false, 5000);
+							setTimeout(() => this.gameFinished = false, 7000);
 						}
 						else{
 							if (this.score2 > this.score1 + 7)
