4108a30e691a837bbf2a251fc3f51b58ba986b92
hanhapyung <peace0704@naver.com>
2022-08-10 00:27:15 +0900

    FE) refactor: teachable machine 컴포넌트화

--- frontend/src/views/RoomView.vue
+++ frontend/src/views/RoomView.vue
@@ -9,6 +9,7 @@
     >
       <div class="d-flex" style="height: 98%; width: 90%">
         <div id="game-container" style="height: 100%; width: 100%"></div>
+				<TeachableItem ref="teachable" @sendScore="sendScore"></TeachableItem>
       </div>
     </div>
     <div
@@ -25,9 +26,15 @@
         >
           <div style="heigth: 100%; width: 23%; background: #123455">
             <user-video :stream-manager="publisher" />
+						<!-- <canvas id="main-video-canvas" style="display:none;"/> -->
           </div>
           <div style="heigth: 100%; width: 49%">
-            <ContentBox :height="100" :width="100"> </ContentBox>
+            <ContentBox :height="100" :width="100">
+							<button class="btn btn-lg btn-success" @click="sendStart()">Start</button>
+							<input class="btn btn-large btn-danger" type="button" id="buttonLeaveSession" @click="audioControl" v-if="!audioMute" value="오디오 중지">
+							<input class="btn btn-large btn-danger" type="button" id="buttonLeaveSession" @click="audioControl" v-if="audioMute" value="오디오 시작">
+							<!-- <div id='label-container'></div> -->
+						</ContentBox>
           </div>
           <div style="heigth: 100%; width: 23%; background: #123455">
             <user-video v-if="subscribers[0] !== null" :stream-manager="subscribers[0]" />
@@ -64,12 +71,10 @@ import { API_BASE_URL } from "@/config";
 import Game from "../game/game.js";
 import GameResultModal from "./GameResultModalView.vue";
 import ContentBox from "@/components/common/ContentBox.vue";
+import TeachableItem from "@/components/Room/TeachableItem.vue"
 
 axios.defaults.headers.post["Content-Type"] = "application/json";
 
-// const URL = 'https://teachablemachine.withgoogle.com/models/fKbC5tFyY/';
-const URL = "https://teachablemachine.withgoogle.com/models/w6iITyYRf/";
-let model, webcam, ctx, labelContainer, maxPredictions;
 
 export default {
   name: "App",
@@ -78,6 +83,7 @@ export default {
     UserVideo,
     GameResultModal,
     ContentBox,
+		TeachableItem,
   },
 
   data() {
@@ -259,8 +265,7 @@ export default {
 			// 'token' parameter should be retrieved and returned by your own backend
 			
 			// this.getToken(this.mySessionId, this.myUserName)
-
-			this.init()
+			
 
 			window.addEventListener('beforeunload', this.leaveSession)
 			//this.game = Game();			//generate phaser game when entering session
@@ -305,7 +310,9 @@ export default {
 					}
 		},
 		sendStart () {
-			console.log("왔음")
+			console.log("게임시작")
+			// console.log(this.$refs.teachable)
+			this.$refs.teachable.init()
 			this.game.scene.getScene('bootScene').StartScene(1);
 			this.dataInit()
 			this.session.signal({		// 게임 시작 송신
@@ -319,6 +326,19 @@ export default {
 			.catch(error => {
 					console.error(error);
 			})
+		},
+		sendScore( ){
+			this.session.signal({		// 운동 점수 송신
+				data: this.myUserName,  // Any string (optional)
+				to: [],                     // Array of Connection objects (optional. Broadcast to everyone if empty)
+				type: 'score'             // The type of message (optional)
+			})
+			.then(() => {
+					console.log('Message successfully sent');
+			})
+			.catch(error => {
+					console.error(error);
+			});
 		},
 
 		connectSession (token) {
@@ -491,92 +511,7 @@ export default {
           });
       });
     },
-
-    //Methods related to Teachable Machine
-
-    async init() {
-      const modelURL = URL + "model.json";
-      const metadataURL = URL + "metadata.json";
-
-      // load the model and metadata
-      // Refer to tmPose.loadFromFiles() in the API to support files from a file picker
-      model = await tmPose.load(modelURL, metadataURL);
-      maxPredictions = model.getTotalClasses();
-
-      // Convenience function to setup a webcam
-      const flip = true; // whether to flip the webcam
-      webcam = new tmPose.Webcam(200, 200, flip); // width, height, flip
-      await webcam.setup(); // request access to the webcam
-      webcam.play();
-      window.requestAnimationFrame(this.loop);
-
-      // append/get elements to the DOM
-      // append/get elements to the DOM
-      const canvas = document.getElementById("main-video-canvas");
-      canvas.width = 200;
-      canvas.height = 200;
-      ctx = canvas.getContext("2d");
-      labelContainer = document.getElementById("label-container");
-      for (let i = 0; i < maxPredictions; i++) {
-        // and class labels
-        labelContainer.appendChild(document.createElement("div"));
-      }
-    },
-
-    async loop(timestamp) {
-      webcam.update(); // update the webcam frame
-      await this.predict();
-      window.requestAnimationFrame(this.loop);
-    },
-
-    async predict() {
-      // Prediction #1: run input through posenet
-      // estimatePose can take in an image, video or canvas html element
-      const { pose, posenetOutput } = await model.estimatePose(webcam.canvas);
-      // Prediction 2: run input through teachable machine classification model
-      const prediction = await model.predict(posenetOutput);
-      if (prediction[0].probability.toFixed(2) >= 0.99) {
-        if (this.status == 1) {
-          this.session
-            .signal({
-              // 운동 점수 송신
-              data: this.myUserName, // Any string (optional)
-              to: [], // Array of Connection objects (optional. Broadcast to everyone if empty)
-              type: "score", // The type of message (optional)
-            })
-            .then(() => {
-              console.log("Message successfully sent");
-            })
-            .catch((error) => {
-              console.error(error);
-            });
-        }
-        this.status = 0;
-      } else if (prediction[1].probability.toFixed(2) >= 0.99) {
-        this.status = 1;
-      }
-      for (let i = 0; i < maxPredictions; i++) {
-        const classPrediction =
-          prediction[i].className + ": " + prediction[i].probability.toFixed(2);
-        labelContainer.childNodes[i].innerHTML = classPrediction;
-      }
-
-      // finally draw the poses
-      //this.drawPose(pose);
-    },
-
-    drawPose(pose) {
-      ctx.drawImage(webcam.canvas, 0, 0);
-      // draw the keypoints and skeleton
-      if (pose) {
-        const minPartConfidence = 0.5;
-        tmPose.drawKeypoints(pose.keypoints, minPartConfidence, ctx);
-        tmPose.drawSkeleton(pose.keypoints, minPartConfidence, ctx);
-      }
-    },
-
-    //END OF TEACHABLE MACHINE METHODS
-
+		
     ...mapActions(["setRoomClose", "leaveRoom"]),
   },
 
@@ -587,6 +522,7 @@ export default {
       "roomJoin",
       "joinInfo",
       "authHeader",
+			"predictionData"
     ]),
   },
   mounted() {
