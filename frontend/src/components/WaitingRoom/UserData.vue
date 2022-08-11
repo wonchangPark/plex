83765df6150c31322c12c868a611b65cdc05e3e2
hanhapyung <peace0704@naver.com>
2022-08-11 22:22:19 +0900

    FE) feat: 대기방 유저 정보

---
+++ frontend/src/components/WaitingRoom/UserData.vue
@@ -0,0 +1,17 @@
+<template>
+  <ContentBox :height="90" :width="80">
+
+  </ContentBox>
+</template>
+
+<script>
+import ContentBox from "../common/ContentBox.vue";
+export default {
+  name: "UserList",
+  components: { ContentBox },
+}
+</script>
+
+<style>
+
+</style>
