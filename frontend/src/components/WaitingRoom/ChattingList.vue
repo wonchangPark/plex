6a57a9eecccd27b4d1a61eb4b237aa0fc7ff6f2d
Kim YongHwan <ymin96@naver.com>
2022-08-18 08:39:05 +0900

    FE) feat: 대기방 채팅 분기

--- frontend/src/components/WaitingRoom/ChattingList.vue
+++ frontend/src/components/WaitingRoom/ChattingList.vue
@@ -1,18 +1,50 @@
 <template>
-    <ContentBox class="chatting-list" :height="90" :width="100">
-        <div class="d-flex flex-column pt-1 chatting_list" style="height: 100%; width: 100%">
-            <div class="d-flex flex-column" style="height: 87%; width: 100%">
-                <div class="d-flex flex-column chatting-list-box" ref="chattingListBox">
-                    <ChattingItem v-for="(item, index) in recvList" :key="index" :name="item.userName" :content="item.content" :img="item.img"></ChattingItem>
-                </div>
-            </div>
-            <div class="d-flex flex-row align-center" style="height: 13%; width: 100%">
-                <button class="mx-2 chatting-sub text--primary" @click="toggle">{{ isAll ? "전체" : "방" }}</button>
-                <input class="chatting-input white" v-model="message" @keyup.enter="sendEvent" type="text" />
-                <button class="mx-2 chatting-submit primary" @click="sendEvent">전송</button>
-            </div>
+  <ContentBox class="chatting-list" :height="90" :width="100">
+    <div
+      class="d-flex flex-column pt-1 chatting_list"
+      style="height: 100%; width: 100%"
+    >
+      <div class="d-flex flex-column" style="height: 87%; width: 100%">
+        <div class="d-flex flex-column chatting-list-box" ref="chattingListBox">
+          <div v-if="isAll">
+            <ChattingItem
+              v-for="(item, index) in recvList"
+              :key="index"
+              :name="item.userName"
+              :content="item.content"
+              :img="item.img"
+            ></ChattingItem>
+          </div>
+          <div v-if="!isAll">
+            <ChattingItem
+              v-for="(item, index) in roomRecvList"
+              :key="index"
+              :name="item.userName"
+              :content="item.content"
+              :img="item.img"
+            ></ChattingItem>
+          </div>
         </div>
-    </ContentBox>
+      </div>
+      <div
+        class="d-flex flex-row align-center"
+        style="height: 13%; width: 100%"
+      >
+        <button class="mx-2 chatting-sub text--primary" @click="toggle">
+          {{ isAll ? "전체" : "방" }}
+        </button>
+        <input
+          class="chatting-input white"
+          v-model="message"
+          @keyup.enter="sendEvent"
+          type="text"
+        />
+        <button class="mx-2 chatting-submit primary" @click="sendEvent">
+          전송
+        </button>
+      </div>
+    </div>
+  </ContentBox>
 </template>
 
 <script>
@@ -23,112 +55,116 @@ const RoomStore = "roomStore";
 const SocketStore = "socketStore";
 const Room = "room";
 export default {
-    name: "ChattingList",
-    components: { ContentBox, ChattingItem },
-    data() {
-        return {
-            userName: "",
-            message: "",
-            recvList: [],
-            prevScrollHeight: 0,
-            isAll: true,
-        };
+  name: "ChattingList",
+  components: { ContentBox, ChattingItem },
+  data() {
+    return {
+      userName: "",
+      message: "",
+      recvList: [],
+      prevScrollHeight: 0,
+    };
+  },
+  created: function () {
+    this.userName = this.getUser.nick;
+    if (this.stompClient && this.stompClient.connected) {
+      this.stompClient.subscribe("/send", (res) => {
+        this.receive(JSON.parse(res.body));
+      });
+      window.addEventListener("beforeunload", this.exit);
+      this.send("enter", this.getUser.nick, "", this.getUser.img);
+    }
+    // this.prevScrollHeight = this.$refs.chattingListBox.scrollHeight - this.$refs.chattingListBox.clientHeight;
+  },
+  mounted: function () {
+    this.prevScrollHeight =
+      this.$refs.chattingListBox.scrollHeight -
+      this.$refs.chattingListBox.clientHeight;
+  },
+  beforeDestroy: function () {
+    window.removeEventListener("beforeunload", this.exit);
+    this.exit();
+  },
+  methods: {
+    ...mapMutations(RoomStore, ["ADD_CONNECT_USER", "REMOVE_CONNECT_USER"]),
+    ...mapActions(RoomStore, ["getConnectUsers"]),
+    ...mapMutations(Room, ["sendAll"]),
+    toggle() {
+      this.isAll = this.isAll ? false : true;
     },
-    created: function () {
-        this.userName = this.getUser.nick;
-        if (this.stompClient && this.stompClient.connected) {
-            this.stompClient.subscribe("/send", (res) => {
-                this.receive(JSON.parse(res.body));
-            });
-            window.addEventListener("beforeunload", this.exit);
-            this.send("enter", this.getUser.nick, "", this.getUser.img);
-        }
-        // this.prevScrollHeight = this.$refs.chattingListBox.scrollHeight - this.$refs.chattingListBox.clientHeight;
+    receive({ type, content, userName, img }) {
+      if (type === "message") this.recvList.push({ userName, content, img });
+      else if (type === "enter") {
+        this.getConnectUsers();
+      } else if (type === "exit") {
+        this.getConnectUsers();
+      }
     },
-    mounted: function () {
-        this.prevScrollHeight = this.$refs.chattingListBox.scrollHeight - this.$refs.chattingListBox.clientHeight;
+    sendEvent() {
+      const msg = {
+        type: "message",
+        userName: this.getUser.nick,
+        content: this.message,
+        img: this.getUser.img,
+      };
+      this.sendAll(msg);
+      this.message = "";
+      this.getConnectUsers();
     },
-    beforeDestroy: function () {
-        window.removeEventListener("beforeunload", this.exit);
-        this.exit();
+    exit() {
+      this.send("exit", this.getUser.nick, "", this.getUser.img);
     },
-    methods: {
-        ...mapMutations(RoomStore, ["ADD_CONNECT_USER", "REMOVE_CONNECT_USER"]),
-        ...mapActions(RoomStore, ["getConnectUsers"]),
-        toggle() {
-            this.isAll = this.isAll ? false : true;
-        },
-        send(type, userName, content, img) {
-            //console.log("Send Message:" + content);
-            if (this.stompClient && this.stompClient.connected) {
-                const msg = {
-                    type,
-                    userName,
-                    content,
-                    img,
-                };
-                this.stompClient.send("/receive", JSON.stringify(msg), {});
-            }
-        },
-        receive({ type, content, userName, img }) {
-            if (type === "message") this.recvList.push({ userName, content, img });
-            else if (type === "enter") {
-                this.getConnectUsers();
-            } else if (type === "exit") {
-                this.getConnectUsers();
-            }
-        },
-        sendEvent() {
-            this.send("message", this.getUser.nick, this.message, this.getUser.img);
-            this.message = "";
-            this.getConnectUsers();
-        },
-        exit() {
-            this.send("exit", this.getUser.nick, "", this.getUser.img);
-        },
-    },
-    computed: {
-        ...mapState(["token", "auth"]),
-        ...mapGetters(["getUser", "authHeader"]),
-        ...mapState(SocketStore, ["stompClient", "connected"]),
-        ...mapState(Room, ["room"]),
-    },
-    updated() {
-        if (Math.abs(this.prevScrollHeight - this.$refs.chattingListBox.scrollTop) < 5) {
-            this.$refs.chattingListBox.scrollTop = this.$refs.chattingListBox.scrollHeight - this.$refs.chattingListBox.clientHeight;
-        }
-        this.prevScrollHeight = this.$refs.chattingListBox.scrollHeight - this.$refs.chattingListBox.clientHeight;
-    },
+  },
+  computed: {
+    ...mapState(["token", "auth"]),
+    ...mapGetters(["getUser", "authHeader"]),
+    ...mapState(SocketStore, ["stompClient", "connected"]),
+    ...mapState(Room, ["room", "allRecvList", "roomRecvList", "isAll"]),
+  },
+  updated() {
+    if (
+      Math.abs(this.prevScrollHeight - this.$refs.chattingListBox.scrollTop) < 5
+    ) {
+      this.$refs.chattingListBox.scrollTop =
+        this.$refs.chattingListBox.scrollHeight -
+        this.$refs.chattingListBox.clientHeight;
+    }
+    this.prevScrollHeight =
+      this.$refs.chattingListBox.scrollHeight -
+      this.$refs.chattingListBox.clientHeight;
+  },
 };
 </script>
 <style scoped>
 .chatting-list-box {
-    height: 100%;
-    width: 100%;
-    overflow-y: scroll;
+  height: 100%;
+  width: 100%;
+  overflow-y: scroll;
 }
 
 .chatting-list-box::-webkit-scrollbar {
-    display: none;
+  display: none;
 }
 
 .chatting-list .chatting-input {
-    height: 80%;
-    width: 90%;
-    border-radius: 5px;
+  height: 80%;
+  width: 90%;
+  border-radius: 5px;
 }
 
 .chatting-list .chatting-submit {
-    height: 80%;
-    width: 5%;
-    border-radius: 5px;
-    font-weight: bold;
+  height: 80%;
+  width: 5%;
+  border-radius: 5px;
+  font-weight: bold;
+  font-size: 0.8vw;
 }
 
 .chatting-list .chatting-sub {
-    height: 80%;
-    width: 5%;
-    border-radius: 5px;
-    font-weight: bold;
+  height: 80%;
+  width: 5%;
+  border-radius: 5px;
+  font-weight: bold;
+  font-size: 0.8vw;
 }
 </style>
--- frontend/src/store/modules/room.js
+++ frontend/src/store/modules/room.js
@@ -1,142 +1,169 @@
 import { createRoomApi, leaveRoomApi, setRoomUserApi } from "@/api/room.js";
 import router from "@/router";
 import axios from "axios";
-import { API_BASE_URL } from '@/config';
+import { API_BASE_URL } from "@/config";
 
-const API_URL = API_BASE_URL + '/api/v1';
+const API_URL = API_BASE_URL + "/api/v1";
 
 const room = {
-    namespaced: true,
-    state: {
-        room: {},
-        roomJoin: false,
-        users: [],
+  namespaced: true,
+  state: {
+    room: {},
+    roomJoin: false,
+    users: [],
+    allRecvList: [],
+    roomRecvList: [],
+    isAll: true,
+  },
+  getters: {
+    roomJoin: (state) => state.roomJoin,
+  },
+  mutations: {
+    SET_ROOM: (state, room) => {
+      state.room = room;
     },
-    getters: {
-        roomJoin: state => state.roomJoin
+    INIT_USERS: (state) => {
+      state.users = [];
     },
-    mutations: {
-        SET_ROOM: (state, room) => {
-            state.room = room;
-        },
-        INIT_USERS: (state) => {
-            state.users = [];
-        },
-        INIT_HOST: (state, user) => {
-            state.users = [];
-            state.users.push(user);
-        },
-        SET_USERS: (state, users) => {
-            state.users = users;
-        },
-        DELETE_USER: (state, nick) => {
-            state.users = state.users.filter((user) => user.nick !== nick);
-        },
-        ADD_USER: (state, user) => {
-            state.users.push(user);
-        },
-        INIT_ROOM: (state) => {
-            state.room = {};
-        },
-        SET_ROOMJOIN: (state) => {state.roomJoin = true},
-        SET_ROOMCLOSE: (state) => {
-            state.roomJoin = false
-          },
-        UPDATE_USER: (state, user) => {
-            state.users = state.users.map((item) => {
-                console.log(item);
-                console.log(user);
-                if (item.nick === user.nick) return user;
-                return item;
-            });
-        },
-        INIT_TEAM_INFO: (state) => {
-            state.users = state.users.map((user) => {
-                user.team = 0;
-                return user;
-            });
-        },
+    INIT_HOST: (state, user) => {
+      state.users = [];
+      state.users.push(user);
     },
-    actions: {
-        roomCreate({ rootState, commit }, roomInfo) {
-            createRoomApi(
-                { headers: rootState.auth.authHeader, roomInfo },
-                ({ data }) => {
-                    console.log(data);
-                    commit("SET_ROOM", data);
-                    let user = {
-                        nick: rootState.auth.user.nick,
-                        team: 0,
-                        host: true,
-                        userId: rootState.auth.user.userId,
-                        img: rootState.auth.user.img,
-                    };
-                    commit("INIT_HOST", user);
-                    router.push("/waiting/" + data.code);
-                },
-                (error) => {
-                    console.log(error);
-                }
-            );
-        },
-        leaveRoom({ rootState, commit }, joinInfo) {
-            leaveRoomApi(
-                { headers: rootState.auth.authHeader, joinInfo },
-                () => {
-                    commit("DELETE_USER", joinInfo.id);
-                },
-                (error) => {
-                    console.log(error);
-                }
-            );
-        },
-        joinRoom({ rootState, commit }, roomCode) {
-            setRoomUserApi(
-                { headers: rootState.auth.authHeader, code: roomCode, id: rootState.auth.user.nick },
-                ({ data }) => {
-                    commit("SET_ROOM", data);
-                    router.push("/waiting/" + data.code);
-                },
-                (error) => {
-                    console.log(error);
-                    if (error.response.status == 403) {
-                        alert("이미 인원이 다 차있어 입장 불가능합니다.");
-                    } else if (error.response.status == 406) {
-                        alert("이미 해당 아이디가 대기방에 들어가 있습니다.");
-                    }
-                }
-            );
-        },
-        setGameHistory({ dispatch, rootState}, { roomNo, score }) {
-            axios({
-              url: API_URL + `/rooms/game?roomNo=${roomNo}`,
-              method: 'post',
-              headers: rootState.auth.authHeader
-            })
-            .then( (res) => {
-              console.log(res)
-              score.gameHistoryNo = res.data
-              dispatch('setGameScore', score)
-            })
-            .catch((e) => {
-              console.log(e)
-            })
-          },
-          setGameScore({ rootState }, score) {
-            axios({
-              url: API_URL + '/rooms/score',
-              method: 'post',
-              data: score,
-              headers: rootState.auth.authHeader
-            })
-            .then( (res) => {
-              console.log(res)
-            })
-            .catch((e) => {
-              console.log(e)
-            })
+    SET_USERS: (state, users) => {
+      state.users = users;
+    },
+    DELETE_USER: (state, nick) => {
+      state.users = state.users.filter((user) => user.nick !== nick);
+    },
+    ADD_USER: (state, user) => {
+      state.users.push(user);
+    },
+    INIT_ROOM: (state) => {
+      state.room = {};
+    },
+    SET_ROOMJOIN: (state) => {
+      state.roomJoin = true;
+    },
+    SET_ROOMCLOSE: (state) => {
+      state.roomJoin = false;
+    },
+    UPDATE_USER: (state, user) => {
+      state.users = state.users.map((item) => {
+        console.log(item);
+        console.log(user);
+        if (item.nick === user.nick) return user;
+        return item;
+      });
+    },
+    INIT_TEAM_INFO: (state) => {
+      state.users = state.users.map((user) => {
+        user.team = 0;
+        return user;
+      });
+    },
+    INIT_ALL_RECV: (state) => {
+      state.allRecvList = [];
+    },
+    INIT_ROOM_RECV: (state) => {
+      state.roomRecvList = [];
+    },
+    ADD_ALL_RECV: (state, msg) => {
+      state.allRecvList.push(msg);
+    },
+    ADD_ROOM_RECV: (state, msg) => {
+      state.roomRecvList.push(msg);
+    },
+    sendAll(state, msg) {
+        //console.log("Send Message:" + content);
+        if (state.isAll && this.stompClient && this.stompClient.connected) {
+          this.stompClient.send("/receive", JSON.stringify(msg), {});
         }
+      }
+  },
+  actions: {
+    roomCreate({ rootState, commit }, roomInfo) {
+      createRoomApi(
+        { headers: rootState.auth.authHeader, roomInfo },
+        ({ data }) => {
+          console.log(data);
+          commit("SET_ROOM", data);
+          let user = {
+            nick: rootState.auth.user.nick,
+            team: 0,
+            host: true,
+            userId: rootState.auth.user.userId,
+            img: rootState.auth.user.img,
+          };
+          commit("INIT_HOST", user);
+          router.push("/waiting/" + data.code);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
     },
+    leaveRoom({ rootState, commit }, joinInfo) {
+      leaveRoomApi(
+        { headers: rootState.auth.authHeader, joinInfo },
+        () => {
+          commit("DELETE_USER", joinInfo.id);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    },
+    joinRoom({ rootState, commit }, roomCode) {
+      setRoomUserApi(
+        {
+          headers: rootState.auth.authHeader,
+          code: roomCode,
+          id: rootState.auth.user.nick,
+        },
+        ({ data }) => {
+          commit("SET_ROOM", data);
+          router.push("/waiting/" + data.code);
+        },
+        (error) => {
+          console.log(error);
+          if (error.response.status == 403) {
+            alert("이미 인원이 다 차있어 입장 불가능합니다.");
+          } else if (error.response.status == 406) {
+            alert("이미 해당 아이디가 대기방에 들어가 있습니다.");
+          }
+        }
+      );
+    },
+    setGameHistory({ dispatch, rootState }, { roomNo, score }) {
+      axios({
+        url: API_URL + `/rooms/game?roomNo=${roomNo}`,
+        method: "post",
+        headers: rootState.auth.authHeader,
+      })
+        .then((res) => {
+          console.log(res);
+          score.gameHistoryNo = res.data;
+          dispatch("setGameScore", score);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
+    setGameScore({ rootState }, score) {
+      axios({
+        url: API_URL + "/rooms/score",
+        method: "post",
+        data: score,
+        headers: rootState.auth.authHeader,
+      })
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
+  },
 };
 
 export default room;
